b4038897c7b38fac2980abade756b2b844ee42c8
jeffren <jeffrenmokamo726@gmail.com>
2023-12-27 13:30:03 +0100

    initialisation de la page formation pour élèves

--- form-eleves.php
+++ form-eleves.php
@@ -11,9 +11,14 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
-    
 
+    <?php 
+        include('menu.php');
+    ?>
     
+    <h1 class="text-3xl">Elit-Avenir offre aussi des formations de vacances pour les élèves</h1>
+
+
 
 
     <script src="https://cdn.tailwindcss.com"></script>
