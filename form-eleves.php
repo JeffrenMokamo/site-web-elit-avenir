66ba48d5ca3a84c8dcffdf4ceba32b8d0b9cae56
jeffren <jeffrenmokamo726@gmail.com>
2023-12-27 14:10:05 +0100

    Fin page formation pour élèves

--- form-eleves.php
+++ form-eleves.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Elit-Avenir:: Formations pour élèves</title>
+    <title>Elite-Avenir:: Formations pour élèves</title>
     <link type="image/png" rel="shortcut icon" href="images/logo-elit.png">
     <link rel="stylesheet" href="css/elit.css">
     <link rel="stylesheet" href="dist/output.css">
@@ -16,11 +16,28 @@
         include('menu.php');
     ?>
     
-    <h1 class="text-3xl">Elit-Avenir offre aussi des formations de vacances pour les élèves</h1>
+    <div class="english-form mx-auto max-w-2xl px-4 pt-10 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pb-24 lg:pt-16">
+            <div class="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
+                <h1 class="text-2xl font-semibold tracking-tight text-gray-900 mb-4 sm:text-3xl">Elite-Avenir donne aussi des formations des vacances pour les élèves</h1>
+                <p class="text-2xl mb-3 font-semibold">Formation en projet d'immigration</p>
+                <p class="text-2xl">Le centre de formation Elit-Avenir a pour objectif d'accompagner les jeunes en les guidants sur la réalisation d'un projet d'immigration à moindre coût et en toute sécurité. De nombreux jeunes sont tentés de poursuivre leurs érudes à l'étranger après l'obtention de leur bac, mais sont souvent victimes d'agences de voyage qui les arnaquent ainsi que leurs parents. Elite Avenir propose d'enseigner aux finalistes comment exécuter de manière autonome un projet d'immigration et éviter d'être victime d'arnaques. Le processus d'immigration n'est pas difficile et le succès peut être atteint en suivant les recommandations nécessaires et en fournissant les documents requis.</p>
+            </div>
+
+        <div class="mt-4 lg:row-span-3 lg:mt-0">
+            <p class="text-2xl font-semibold tracking-tight text-gray-900 mb-3">Formation en entrepreneuriat</p>
+            <p class="text-2xl">La formation entrepreneuriat pour les élèves de 5ème et 6ème des humanités est importante car elle leur permet d'acquérir des compétences en matière de création d'entreprise. Cela peut les aider à développer leur esprit d'entreprise et leur don er une longueur d'avance dans le monde professionnel. De plus, cela peut leur apprendre à être plus autonomes et à prendre des décisions éclairées en matière de carrière.</p>
+
+            <img class="rounded mb-3" src="images/entre.jpg" alt="">
+           
+        </div>
+
+    </div>
 
 
 
-
+    <?php 
+        include('footer.php');
+    ?>
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
 
